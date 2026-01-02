efc28e2764615c35bb7ec6a7194624af5284e8cd
GhostGamedev <tom.static.hosting@gmail.com>
2026-01-02 00:06:46 -0800

    pages for posts

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,8 +3,11 @@ export default function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy({ public: '/' });
 
-  eleventyConfig.addLiquidFilter("myDateString", function(date) {
-    return `${date.getUTCMonth()+1}/${date.getUTCDate()}/${date.getUTCFullYear()}`;
-  });
+  eleventyConfig.addLiquidFilter(
+    'myDateString', 
+    function(date) {
+      return `${date.getUTCMonth()+1}/${date.getUTCDate()}/${date.getUTCFullYear()}`;
+    }
+  );
 
 };
